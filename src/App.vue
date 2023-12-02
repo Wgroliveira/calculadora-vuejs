cffbbeec2d83abd643cdf47e1535f052f053c263
Wgroliveira <129519069+Wgroliveira@users.noreply.github.com>
2023-12-02 15:55:49 -0300

    finalizando o exercício

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,52 @@
 <script setup>
+import Calculadora_script from './components/Calculadora_script.vue';
 
 </script>
 
 <template>
-  
+  <Calculadora_script />
 </template>
 
 <style scoped>
+    #app {
+  font-family: 'Arial', sans-serif;
+}
 
+.container {
+  background-color: #f3f3f3;
+  padding: 20px;
+  border-radius: 8px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+}
+
+.form-group {
+  margin-bottom: 20px;
+}
+
+label {
+  display: block;
+  font-weight: bold;
+  margin-bottom: 5px;
+}
+
+input,
+select {
+  width: 100%;
+  padding: 10px;
+  box-sizing: border-box;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+}
+
+
+input[disabled] {
+  background-color: #eee;
+}
+
+.result {
+  font-size: 1.2em;
+  font-weight: bold;
+  color: #333;
+  margin-top: 10px;
+}
 </style>
---
+++ src/components/Calculadora_script.vue
@@ -0,0 +1,65 @@
+<script>
+export default {
+  data() {
+    return {
+      num1: 0,
+      num2: 0,
+      operation: 'add',
+    };
+  },
+  computed: {
+    calculateResult() {
+      const num1 = parseFloat(this.num1);
+      const num2 = parseFloat(this.num2);
+
+      switch (this.operation) {
+        case 'add':
+          return num1 + num2;
+        case 'subtract':
+          return num1 - num2;
+        case 'multiply':
+          return num1 * num2;
+        case 'divide':
+          return num1 / num2;
+        default:
+          return 0;
+      }
+    },
+  },
+};
+</script>
+
+<template>
+    <div id="app" class="container mt-5">
+        <div class="row">
+            <div class="col-md-4 offset-md-4">
+                <h2 class="mb-4">Calculadora Aritmética</h2>
+                
+                    <div class="form-group mb-3">
+                        <label for="num1">Número 1:</label>
+                        <input v-model="num1" type="number" class="form-control">
+                    </div>
+
+                    <div class="form-group mb-3">
+                        <label for="num2">Número 2:</label>
+                        <input v-model="num2" type="number" class="form-control">
+                    </div>
+
+                    <div class="form-group mb-3">
+                        <label for="operation" class="select-label">Operação:</label>
+                        <select v-model="operation" class="form-control">
+                            <option value="add">+</option>
+                            <option value="subtract">-</option>
+                            <option value="multiply">*</option>
+                            <option value="divide">/</option>
+                        </select>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="result">Resultado:</label>
+                        <input :value="calculateResult" type="text" class="form-control" disabled>
+                    </div>
+            </div>
+        </div>
+    </div>
+</template>
